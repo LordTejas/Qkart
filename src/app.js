1d077af738fadf835c33bb41ad32fa5cffbeb3e0
Nabhan Abdulla <nabhan@criodo.com>
2021-02-11 20:09:16 +0550

    updated changes - 2021-02-11 19:49:16

--- src/app.js
+++ src/app.js
@@ -35,18 +35,14 @@ app.use(cors());
 app.options("*", cors());
 
 
-// v1 api routes
 app.use("/v1", routes);
 
-// send back a 404 error for any unknown api request
 app.use((req, res, next) => {
   next(new ApiError(httpStatus.NOT_FOUND, "Not found"));
 });
 
-// convert error to ApiError, if needed
 app.use(errorConverter);
 
-// handle error
 app.use(errorHandler);
 
 module.exports = app;
