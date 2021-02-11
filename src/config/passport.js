eea9635cfb2be64bc9515435cfdad5ce47157d1d
Nabhan Abdulla <nabhan@criodo.com>
2021-02-11 20:09:13 +0550

    updated changes - 2021-02-11 19:49:13

--- src/config/passport.js
+++ src/config/passport.js
@@ -1,20 +1,19 @@
-const { Strategy: JwtStrategy, ExtractJwt } = require('passport-jwt');
-const config = require('./config');
-const { tokenTypes } = require('./tokens');
-const { User } = require('../models');
+const { Strategy: JwtStrategy, ExtractJwt } = require("passport-jwt");
+const config = require("./config");
+const { tokenTypes } = require("./tokens");
+const { User } = require("../models");
 
-// TODO: CRIO_TASK_MODULE_AUTH - Set configurations and logic to find matching user
-
+// TODO: CRIO_TASK_MODULE_AUTH - Set mechanism to retrieve Jwt token from user request
 /**
  * These config options are required
- * Option 1: jwt secret
- * Option 2: mechanism to fetch jwt token from request
+ * Option 1: jwt secret environment variable set in ".env"
+ * Option 2: mechanism to fetch jwt token from request Authentication header with the "bearer" auth scheme
  */
 const jwtOptions = {
   secretOrKey: config.jwt.secret,
 };
 
-// NOTE - Quiz and debrief mention on tokenTypes.ACCESS <-> Bearer token
+// TODO: CRIO_TASK_MODULE_AUTH - Implement verify callback for passport strategy to find the user whose token is passed
 /**
  * Logic to find the user matching the token passed
  * - If payload type isn't `tokenTypes.ACCESS` return an Error() with message, "Invalid token type" in the callback function
@@ -22,6 +21,9 @@ const jwtOptions = {
  * - If there's a valid user, return the user in the callback function
  * - If user not found, return `false` in the user field in the callback function
  * - If the function errs, return the error in the callback function
+ * 
+ * @param payload - the payload the token was generated with
+ * @param done - callback function
  */
 const jwtVerify = async (payload, done) => {
 };
