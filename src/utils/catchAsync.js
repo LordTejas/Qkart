24c1753b9edea745bee33b6a742aef5d9f35a8b9
Nabhan Abdulla <nabhan@criodo.com>
2021-02-23 15:15:37 +0550

    updated changes - 2021-02-23 14:55:37

--- src/utils/catchAsync.js
+++ src/utils/catchAsync.js
@@ -1,10 +1,12 @@
 /**
- * Catch and forward any error a function throws to the next middleware 
+ * Return a function that catches and forwards any error a function throws to the next middleware 
  * 
  * @param {Function} fn - input function that catchAsync wraps around
  */
-const catchAsync = (fn) => (req, res, next) => {
-  Promise.resolve(fn(req, res, next)).catch((err) => next(err));
-};
+function catchAsync(fn) {
+  return function(req, res, next) {
+    Promise.resolve(fn(req, res, next)).catch((err) => next(err));
+  }
+}
 
 module.exports = catchAsync;
