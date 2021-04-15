60333051b96d7260dd1abdb5cb84e405cd939274
Nabhan Abdulla <nabhan@criodo.com>
2021-04-15 15:50:23 +0550

    updated changes - 2021-04-15 15:30:23

--- frontend/setup.sh
+++ frontend/setup.sh
@@ -2,13 +2,3 @@
 IP_VAR="$(curl http://checkip.amazonaws.com/)"
 CONFIG='{"workspaceIp": "'"$IP_VAR"'"}'
 echo $CONFIG > src/ipConfig.json
-
-
-# Install project dependencies
-rm -rf ~/node_modules
-rm -rf node_modules
-rm -rf ~/.npm 
-rm package-lock.json
-mkdir -p ~/node_modules
-ln -s /home/crio-user/node_modules $PWD/node_modules
-npm install
