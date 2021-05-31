95fffd35b21e50c3a89140e83c2bc185900727c1
Nabhan Abdulla <nabhan@criodo.com>
2021-05-31 21:37:01 +0550

    updated changes - 2021-05-31 21:17:01

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -4,7 +4,8 @@ const ApiError = require("../utils/ApiError");
 
 /**
  * Login with username and password
- * - Utilize userService methods to fetch user object corresponding to the email provided
+ * - Utilize userService method to fetch user object corresponding to the email provided
+ * - Use the User schema's "isPasswordMatch" method to check if input password matches the one user registered with (i.e, hash stored in MongoDB)
  * - If user doesn't exist or incorrect password,
  * throw an ApiError with "401 Unauthorized" status code and message, "Incorrect email or password"
  * - Else, return the user object
