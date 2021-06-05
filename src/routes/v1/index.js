7c0118b74136f52385c84a2e4e75a0eb1816cd53
Nabhan Abdulla <nabhan@criodo.com>
2021-06-05 13:02:54 +0550

    updated changes - 2021-06-05 12:42:54

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -6,6 +6,7 @@ const productRoute = require("./product.route");
 const router = express.Router();
 
 
+// TODO: CRIO_TASK_MODULE_AUTH - Reroute all API requests beginning with the `/v1/auth` route to Express router in auth.route.js 
 router.use("/products", productRoute);
 
 module.exports = router;
