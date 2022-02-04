557c1f32a7736607196cb9cda02a81f7da06660b
Nabhan <nabhan@criodo.com>
2022-02-04 22:21:56 +0550

    updated changes - 2022-02-04 22:01:56

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -21,6 +21,11 @@ const verifyCallback = (req, resolve, reject) => async (err, user, info) => {
  */
 const auth = () => async (req, res, next) => {
   return new Promise((resolve, reject) => {
+    passport.authenticate(
+      "jwt",
+      { session: false },
+      verifyCallback(req, resolve, reject)
+    )(req, res, next);
   })
     .then(() => next())
     .catch((err) => next(err));
